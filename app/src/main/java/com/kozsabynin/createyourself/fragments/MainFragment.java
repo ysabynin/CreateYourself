612fb4daca968fb6686f2d2541876e4d66d4eb55
ysabynin <evgensabyka@mail.ru>
2016-02-28 16:45:04 +0300

    Initial commit

---
+++ app/src/main/java/com/kozsabynin/createyourself/fragments/MainFragment.java
@@ -0,0 +1,26 @@
+package com.kozsabynin.createyourself.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kozsabynin.createyourself.R;
+
+/**
+ * Created by Evgeni Developer on 28.02.2016.
+ */
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+/*
+        return super.onCreateView(inflater, container, savedInstanceState);
+*/
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return rootView;
+    }
+}
