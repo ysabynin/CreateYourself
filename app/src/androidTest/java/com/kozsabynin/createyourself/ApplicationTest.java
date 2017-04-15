bf305ea1b096f44a2459efd3507e23805d5b1c04
ysabynin <evgensabyka@mail.ru>
2017-04-15 22:57:36 +0300

    Added Espresso support

--- app/src/androidTest/java/com/kozsabynin/createyourself/ApplicationTest.java
+++ app/src/androidTest/java/com/kozsabynin/createyourself/ApplicationTest.java
@@ -1,13 +1,55 @@
 package com.kozsabynin.createyourself;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import com.kozsabynin.createyourself.activity.LoginActivity;
+import com.kozsabynin.createyourself.activity.SignupActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.core.deps.guava.base.CharMatcher.is;
+import static android.support.test.espresso.core.deps.guava.base.Predicates.not;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Rule
+    public ActivityTestRule<LoginActivity> mLoginRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
+    @Rule
+    public ActivityTestRule<SignupActivity> mSignupRule =
+            new ActivityTestRule<>(SignupActivity.class);
+
+    @Test
+    public void goToSignUpPage() {
+        mLoginRule.launchActivity(new Intent());
+        onView(withId(R.id.textViewSignUp)).perform(click());
+        onView(withId(R.id.buttonSignup)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void goToLoginPage() {
+        mSignupRule.launchActivity(new Intent());
+        onView(withId(R.id.textViewSignin)).perform(click());
+        onView(withId(R.id.buttonSignin)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkEmptyLogin() {
+        mLoginRule.launchActivity(new Intent());
+        onView(withId(R.id.buttonSignin)).perform(click());
+        onView(withId(R.id.buttonSignin)).check(matches(isDisplayed()));
     }
 }
